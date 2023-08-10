e905c187914ba2f22485b9c4667468c1a39a111a
Alex Wohlbruck <alexwohlbruck@gmail.com>
2023-08-10 14:48:48 -0400

    Add location data disclaimer

--- src/components/LocationPermissionModal.svelte
+++ src/components/LocationPermissionModal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Modal, Alert } from "flowbite-svelte";
+  import { Modal, Alert, P } from "flowbite-svelte";
   import Button from "../lib/Button.svelte";
   import {
     getCurrentLocation,
@@ -40,9 +40,9 @@
 </script>
 
 <Modal bind:open={locationUnavailable} size="xs" permanent>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col gap-5 items-center">
     <svg
-      class="mx-auto w-14 h-14 mb-4 dark:text-gray-200 dark:text-white"
+      class="mx-auto w-14 h-14 dark:text-gray-200 dark:text-white"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
@@ -52,9 +52,17 @@
         d="M7 0a7 7 0 0 0-1 13.92V19a1 1 0 1 0 2 0v-5.08A7 7 0 0 0 7 0Zm0 5.5A1.5 1.5 0 0 0 5.5 7a1 1 0 0 1-2 0A3.5 3.5 0 0 1 7 3.5a1 1 0 0 1 0 2Z"
       />
     </svg>
-    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-      Grant access to your location to get started
-    </h3>
+    <div class="flex flex-col gap-1">
+      <h3
+        class="text-center text-lg font-normal text-gray-500 dark:text-gray-400"
+      >
+        Grant access to your location to get started
+      </h3>
+      <P size="sm" class="text-center">
+        Your location data will not be uploaded or shared with anyone. It is
+        kept locally on your device.
+      </P>
+    </div>
     {#if status === "denied"}
       <Alert class="text-center">
         <span class="font-medium"
