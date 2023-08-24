23992c669db88eec21cf21b6d9df874cd0c16b52
Alex Wohlbruck <alexwohlbruck@gmail.com>
2023-08-24 17:54:13 -0400

    Create stepper component

---
+++ src/components/Stepper.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import { Button, Indicator } from "flowbite-svelte";
+  import { Icon } from "flowbite-svelte-icons";
+
+  export let steps: any[] = [];
+
+  let currentStep = 0;
+  $: indicators = steps.map((step, i) => {
+    return {
+      completed: i < currentStep,
+    };
+  });
+
+  $: canNext = currentStep < steps.length - 1;
+  $: canPrev = currentStep > 0;
+
+  function next() {
+    if (canNext) {
+      currentStep++;
+    }
+  }
+
+  function prev() {
+    if (canPrev) {
+      currentStep--;
+    }
+  }
+</script>
+
+<div class="flex flex-col gap-4">
+  <div class="flex items-center">
+    <div class:invisible={!canPrev}>
+      <Button on:click={prev} color="none" size="xs">
+        <Icon size="sm" name="chevron-left-outline" class="text-gray-500" />
+      </Button>
+    </div>
+
+    <div class="flex-1">
+      {#each steps as component, i}
+        {#if i === currentStep}
+          <svelte:component this={component} />
+        {/if}
+      {/each}
+    </div>
+
+    <div class:invisible={!canNext}>
+      <Button on:click={next} color="none" size="xs">
+        <Icon size="sm" name="chevron-right-outline" class="text-gray-500" />
+      </Button>
+    </div>
+  </div>
+
+  <div class="flex items-center gap-2 px-2">
+    {#each indicators as indicator, i}
+      <Indicator size="xl" color="none">
+        <Icon
+          name={indicator.completed
+            ? "check-circle-solid"
+            : "check-circle-outline"}
+          class={indicator.completed
+            ? "text-emerald-500"
+            : i === currentStep
+            ? "text-primary-500"
+            : "text-gray-500"}
+        />
+      </Indicator>
+      {#if i !== indicators.length - 1}
+        <div class="flex w-full bg-gray-200 h-0.5 dark:bg-gray-700" />
+      {/if}
+    {/each}
+  </div>
+</div>
