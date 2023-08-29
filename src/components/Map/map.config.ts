d2321e86e65e38f0886531570730e045e99f2af8
Alex Wohlbruck <alexwohlbruck@gmail.com>
2023-08-28 23:27:08 -0400

    Update default zoom for geolocate control

--- src/components/Map/map.config.ts
+++ src/components/Map/map.config.ts
@@ -5,7 +5,7 @@ export const DEFAULT_FETCH_RADIUS = 5000;
 export const INITIAL_STATE = {
   lng: -30.6271504,
   lat: 29.108255,
-  zoom: 2,
+  zoom: 1,
 };
 export const RACKS_LAYER_MAX_ZOOM = 13;
 export const RACKS_FETCH_OUTER_BOUNDS_RATIO = (radius) => 2.0 * radius + 1000;
@@ -35,7 +35,7 @@ export const geolocateControlConfig = {
   trackUserLocation: true,
   showUserHeading: true,
   fitBoundsOptions: {
-    maxZoom: 18,
+    maxZoom: 16,
   },
 };
 
