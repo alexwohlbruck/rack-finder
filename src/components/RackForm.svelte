8c851854132e69b2d51e38c0d349b15b3daba963
Alex Wohlbruck <alexwohlbruck@gmail.com>
2023-10-17 16:39:25 -0400

    Update rack type selection to radio tiles

--- src/components/RackForm.svelte
+++ src/components/RackForm.svelte
@@ -7,7 +7,7 @@
     P,
     Select,
     Spinner,
-    Button as FlowbiteButton,
+    Radio,
     Textarea,
   } from "flowbite-svelte";
   import Button from "../lib/Button.svelte";
@@ -25,6 +25,7 @@
   import { location } from "svelte-spa-router";
   import { prefsStore } from "../store/prefs";
   import { ArrowLeftSolid } from "flowbite-svelte-icons";
+  import RackIcon from "../lib/icons/RackIcon.svelte";
 
   const { getMap } = getContext(key) as any;
   const map = getMap();
@@ -99,14 +100,14 @@
       value: "wall_loops",
       name: "Wall loops",
     },
-    {
-      value: "building",
-      name: "Building",
-    },
-    {
-      value: "two-tier",
-      name: "Two tier",
-    },
+    // {
+    //   value: "building",
+    //   name: "Building",
+    // },
+    // {
+    //   value: "two-tier",
+    //   name: "Two tier",
+    // },
   ];
   const accessOptions = [
     {
@@ -210,15 +211,26 @@
   </div>
 
   <Label for="type">
-    <div class="mb-1">{$t("rack.attributes.type")}</div>
-    <Select
-      bind:value={form.bicycle_parking}
-      id="type"
-      items={rackTypeOptions}
-      required
-      placeholder={$t("common.chooseOption")}
-    />
+    <div>{$t("rack.attributes.type")}</div>
   </Label>
+
+  <div class="grid gap-2 w-full md:grid-cols-4">
+    {#each rackTypeOptions as type}
+      <Radio
+        name="custom"
+        custom
+        value={type.value}
+        bind:group={form.bicycle_parking}
+      >
+        <div
+          class="inline-flex flex-col justify-center items-center gap-1 p-2 w-full text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-primary-500 peer-checked:border-primary-600 peer-checked:text-primary-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+        >
+          <RackIcon name={type.value} capacity="1" size={6} />
+          <P size="xs" weight="medium">{type.name}</P>
+        </div>
+      </Radio>
+    {/each}
+  </div>
 
   <Label for="capacity">
     <div class="mb-1">{$t("rack.attributes.capacity")}</div>
